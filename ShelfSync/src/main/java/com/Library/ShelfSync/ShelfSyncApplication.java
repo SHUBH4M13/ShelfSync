a78d9af67ea3c010069642198364d8dd7b849072
Shubham Karna <skarna230@gmail.com>
2026-06-30 23:50:23 +0530

    first commit

---
+++ ShelfSync/src/main/java/com/Library/ShelfSync/ShelfSyncApplication.java
@@ -0,0 +1,13 @@
+package com.Library.ShelfSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfSyncApplication.class, args);
+	}
+
+}
---
+++ ShelfSync/src/test/java/com/Library/ShelfSync/ShelfSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Library.ShelfSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
