79a9f1599c145c9472ba0e8b378f0eb877fb4998
Shubham Karna <skarna230@gmail.com>
2026-07-08 16:08:39 +0530

    author service done

--- src/main/java/com/Library/ShelfSync/controller/AuthorController.java
+++ src/main/java/com/Library/ShelfSync/controller/AuthorController.java
@@ -5,9 +5,10 @@ import com.Library.ShelfSync.models.AuthorEntity;
 import com.Library.ShelfSync.services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class AuthorController {
@@ -17,8 +18,31 @@ public class AuthorController {
 
     @PreAuthorize("hasAnyRole('LIBRARIAN' , 'ADMIN')")
     @PostMapping("/api/author/create")
-    AuthorEntity HandleCreateAuthor(@RequestBody AuthorRequest authorRequest){
-        return authorService.HandleCreateAuthor(authorRequest);
+    AuthorEntity handleCreateAuthor(@RequestBody AuthorRequest authorRequest){
+        return authorService.handleCreateAuthor(authorRequest);
     }
+
+    @GetMapping("/api/author/all")
+    List<AuthorEntity> handleGetAllAuthor(){
+        return authorService.handleGetAllAuthor();
+    }
+
+    @GetMapping("/api/author/{id}")
+    AuthorEntity handleGetAuthor(@PathVariable Long id){
+        return authorService.handleGetAuthor(id);
+    }
+
+    @PreAuthorize("hasAnyRole('LIBRARIAN' , 'ADMIN')")
+    @PatchMapping("/api/author/edit/{id}")
+    AuthorEntity handleEditAuthor(@PathVariable Long id){
+
+    }
+
+    @PreAuthorize("hasAnyRole('LIBRARIAN' , 'ADMIN')")
+    @DeleteMapping("/api/author/delete/{id}")
+    Long handleDeleteAuthor(@PathVariable Long id){
+        return authorService.handleDeleteAuthor(id);
+    }
+
 
 }
--- src/main/java/com/Library/ShelfSync/services/AuthorService.java
+++ src/main/java/com/Library/ShelfSync/services/AuthorService.java
@@ -3,9 +3,14 @@ package com.Library.ShelfSync.services;
 import com.Library.ShelfSync.dto.AuthorRequest;
 import com.Library.ShelfSync.models.AuthorEntity;
 import com.Library.ShelfSync.repository.AuthorRepo;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AuthorService {
@@ -13,7 +18,7 @@ public class AuthorService {
     @Autowired
     private AuthorRepo authorRepo;
 
-    public AuthorEntity HandleCreateAuthor(AuthorRequest authorRequest){
+    public AuthorEntity handleCreateAuthor(@NotNull AuthorRequest authorRequest){
 
         if(authorRepo.existsByName(authorRequest.getName())){
             throw new RuntimeException("Author already exists");
@@ -25,6 +30,35 @@ public class AuthorService {
         return authorRepo.save(author);
     }
 
+    public List<AuthorEntity> handleGetAllAuthor(){
+        return authorRepo.findAll();
+    }
+
+    public AuthorEntity handleGetAuthor(Long id){
+        return authorRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Author not found"));
+    }
+
+    public AuthorEntity handleEditAuthor(Long id , AuthorRequest authorRequest){
+
+        AuthorEntity author = authorRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Author not found"));
+
+        author.setName(authorRequest.getName());
+
+        return authorRepo.save(author);
+
+    }
+
+
+    public Long handleDeleteAuthor(Long id){
+        AuthorEntity author = authorRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Author not found"));
+
+        authorRepo.delete(author);
+
+        return id;
+    }
 
 
 }
